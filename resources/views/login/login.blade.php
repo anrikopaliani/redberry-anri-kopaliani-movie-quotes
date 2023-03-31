7dd00dbf38493df92030c37d7bdcd4275c7ea770
Anri <anrikopa@gmail.com>
2023-03-31 22:52:42 +0400

    style:password type on input

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -2,7 +2,7 @@
     <form action="#">
         @csrf
         <x-input name="username" />
-        <x-input name="password" />
+        <x-input name="password" type="password" />
         <x-submit-button>Submit</x-submit-button>
     </form>
 </x-layout>
