b8651ad8434464ba42605c304b9b526629a18668
Anri <anrikopa@gmail.com>
2023-04-02 00:53:20 +0400

    style:error messages on inputs

--- resources/views/add-movie-form/movie-form.blade.php
+++ resources/views/add-movie-form/movie-form.blade.php
@@ -1,11 +1,8 @@
 <x-layout>
     <h1 class="text-center text-8xl">Add a Movie</h1>
-    <form action="" method="POST" class="mx-auto mb-28">
+    <form action="/movie-form" method="POST" class="mx-auto mb-28" enctype="multipart/form-data">
+        @csrf
         <x-input name="title" />
-        <div class="flex flex-col space-y-2 ">
-            <label for="image">Image</label>
-            <input class="text-white" type="file" accept="image/*" name="image" id="image">
-        </div>
         <x-submit-button>Submit</x-submit-button>
     </form>
 </x-layout>
--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -4,4 +4,7 @@
     <label for="{{ $name }}">{{ ucwords($name) }}</label>
     <input value="{{ $type === 'text' ? old($name) : '' }}" class="border rounded-md h-10 text-black"
         type="{{ $type }}" id="{{ $name }}" name="{{ $name }}">
+    @error($name)
+        <p class=" text-red-600">{{ $message }}</p>
+    @enderror
 </div>
