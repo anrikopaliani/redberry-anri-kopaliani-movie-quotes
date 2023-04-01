2892e4b9edba5d1e3651e9d4ff960679c9fdda44
Anri <anrikopa@gmail.com>
2023-04-02 00:44:03 +0400

    feat:create movie from controller

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreMovieRequest;
+use App\Models\Movie;
+
 class MovieController extends Controller
 {
 	public function index()
 	{
 		return view('add-movie-form.movie-form');
+	}
+
+	public function store(StoreMovieRequest $request)
+	{
+		$validated = $request->validated();
+		$validated['user_id'] = auth()->user()->id;
+		Movie::create($validated);
+
+		return redirect('/');
 	}
 }
