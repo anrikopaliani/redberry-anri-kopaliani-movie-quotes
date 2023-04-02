6f90a9e76147bd159b03ca50c8b14649531ee305
Anri <anrikopa@gmail.com>
2023-04-02 22:40:06 +0400

    fix:create user_id column on quotes table

--- database/migrations/2023_03_30_215848_create_quotes_table.php
+++ database/migrations/2023_03_30_215848_create_quotes_table.php
@@ -14,6 +14,7 @@ return new class extends Migration {
 			$table->id();
 			$table->string('quote');
 			$table->foreignId('movie_id')->references('id')->on('movies');
+			$table->foreignId('user_id')->references('id')->on('users');
 			$table->timestamps();
 		});
 	}
