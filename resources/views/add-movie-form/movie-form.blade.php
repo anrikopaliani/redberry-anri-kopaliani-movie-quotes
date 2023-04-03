ca47371e44d715f54f7428359e1b0c4ba92bb58d
Anri <anrikopa@gmail.com>
2023-04-03 12:38:51 +0400

    fix:remove enctype

--- resources/views/add-movie-form/movie-form.blade.php
+++ resources/views/add-movie-form/movie-form.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <h1 class="text-center text-8xl">Add a Movie</h1>
-    <form action="/movie-form" method="POST" class="mx-auto mb-28" enctype="multipart/form-data">
+    <form action="/movie-form" method="POST" class="mx-auto mb-28">
         @csrf
         <x-input name="title" />
         <x-submit-button>Submit</x-submit-button>
