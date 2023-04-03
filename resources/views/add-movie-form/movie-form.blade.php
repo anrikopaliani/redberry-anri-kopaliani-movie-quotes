86dae95f2d84ac8bdb05de9b75bb697a0343d3d8
Anri <anrikopa@gmail.com>
2023-04-03 12:48:27 +0400

    fix:named routes

--- resources/views/add-movie-form/movie-form.blade.php
+++ resources/views/add-movie-form/movie-form.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <h1 class="text-center text-8xl">Add a Movie</h1>
-    <form action="/movie-form" method="POST" class="mx-auto mb-28">
+    <form action="{{ route('movie') }}" method="POST" class="mx-auto mb-28">
         @csrf
         <x-input name="title" />
         <x-submit-button>Submit</x-submit-button>
