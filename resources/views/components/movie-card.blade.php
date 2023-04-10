d42002eb00cb178780c8c84b524180bed3e79175
Anri <anrikopa@gmail.com>
2023-04-11 00:58:30 +0400

    feat:randomly show movies

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -1,10 +1,9 @@
 @props(['movie'])
 
 <div class="flex flex-col">
-    <img width="700" height="386px" class="mb-6"
-        src="https://media.istockphoto.com/id/1406127371/photo/women-wearing-3d-glasses-watching-a-movie-and-her-giant-popcorn-3d-render-illustration.jpg?b=1&s=170667a&w=0&k=20&c=sOe1e0SkxCmB6HsDN_NbzH9hPGhQpzXAXesWs2a7jTA="
-        alt="">
-    <p class="text-center text-5xl mb-12">{{ $movie }}</p>
+    <img width="600" height="386px" class="mb-6 mx-auto"
+        src="{{ URL::to('/') . '/storage/' . str_replace('images/', '', $movie->image) }}" alt="">
+    <p class="text-center text-5xl mb-12">{{ $movie->getTranslation('quote', 'en') }}</p>
 
-    <a href="#" class="text-center underline text-5xl">shawshank redemption</a>
+    <a href="/movies/{{ $movie->movie_id }}" class="text-center underline text-5xl">{{ $movie->movie->title }}</a>
 </div>
