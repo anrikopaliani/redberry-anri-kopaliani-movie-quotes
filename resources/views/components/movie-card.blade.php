fc3c48e93f6f44f2cfa82a08225d3e94c73d3c96
Anri <anrikopa@gmail.com>
2023-04-11 01:43:20 +0400

    feat:movie translation on main page

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -3,7 +3,7 @@
 <div class="flex flex-col">
     <img width="600" height="386px" class="mb-6 mx-auto"
         src="{{ URL::to('/') . '/storage/' . str_replace('images/', '', $movie->image) }}" alt="">
-    <p class="text-center text-5xl mb-12">{{ $movie->getTranslation('quote', 'en') }}</p>
+    <p class="text-center text-5xl mb-12">{{ $movie->getTranslation('quote', app()->getLocale()) }}</p>
 
     <a href="/movies/{{ $movie->movie_id }}" class="text-center underline text-5xl">{{ $movie->movie->title }}</a>
 </div>
