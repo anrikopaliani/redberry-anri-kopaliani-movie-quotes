f36f7d0b4c1c500f6ad3555417b0fcc9fd378241
anrikopaliani <anrikopa@gmail.com>
2023-04-20 01:51:55 +0400

    fix:see the listing page without the log in

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::get('/', [RandomQuoteController::class, 'index']);
 
 Route::get('language/{locale}', [StaticLanguageController::class, 'store']);
 
+Route::get('movies/{movie}', [MovieController::class, 'show']);
+
 Route::middleware('guest')->group(function () {
 	Route::get('login', [LoginController::class, 'index'])->name('login.get');
 	Route::post('login', [LoginController::class, 'store'])->name('login.post');
@@ -34,8 +36,6 @@ Route::middleware('auth')->group(function () {
 
 	Route::get('/add-quote', [QuotesController::class, 'index'])->name('add-quote.get');
 	Route::post('/add-quote', [QuotesController::class, 'store'])->name('add-quote.post');
-
-	Route::get('movies/{movie}', [MovieController::class, 'show']);
 
 	Route::prefix('quotes')->controller(QuotesController::class)->group(function () {
 		Route::delete('/{quote}', 'destroy');
