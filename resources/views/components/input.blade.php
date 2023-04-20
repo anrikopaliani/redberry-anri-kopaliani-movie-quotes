8a86f3fd218452779bc7571e4ef0236dd57fa1d6
anrikopaliani <anrikopa@gmail.com>
2023-04-20 17:25:41 +0400

    fix:add padding to inputs

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -2,7 +2,7 @@
 
 <div class="flex flex-col space-y-2 w-96 mb-4">
     <label for="{{ $name }}">{{ ucwords($label) }}</label>
-    <input value="{{ $type === 'text' ? old($name) : '' }}" class="border rounded-md h-10 text-black"
+    <input value="{{ $type === 'text' ? old($name) : '' }}" class="border rounded-md h-10 text-black px-4"
         type="{{ $type }}" id="{{ $name }}" name="{{ $name }}">
     @error($name)
         <p class=" text-red-600">{{ $message }}</p>
