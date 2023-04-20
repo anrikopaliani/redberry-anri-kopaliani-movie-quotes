ee674ea1f29be8d2378fdbcd2f1f2c003840677d
anrikopaliani <anrikopa@gmail.com>
2023-04-20 18:44:18 +0400

    refactor:move to ka folder

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -1,8 +1,8 @@
 <x-layout>
     <form action="/login" method="POST">
         @csrf
-        <x-input label="{{ __('username') }}" name="username" />
-        <x-input label="{{ __('password') }}" name="password" type="password" />
+        <x-input label="{{ __('messages.username') }}" name="username" />
+        <x-input label="{{ __('messages.password') }}" name="password" type="password" />
         <x-submit-button>{{ __('LOG IN') }}</x-submit-button>
 
     </form>
