ccca03e343dadc871ecd55e8981fd8ea770d3b69
anrikopaliani <anrikopa@gmail.com>
2023-04-21 15:10:00 +0400

    fix:format long text

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -1,9 +1,10 @@
 @props(['quote'])
 
-<div class="flex flex-col">
-    <img width="600" height="386px" class="mb-6 mx-auto"
+<div class="flex flex-col h-screen items-center" style="width: 1000px">
+    <img width="600" height="386px" class="mt-28 mb-6 mx-auto"
         src="{{ URL::to('/') . '/storage/' . str_replace('images/', '', $quote->image) }}" alt="">
-    <p class="text-center text-3xl mb-12">{{ $quote->getTranslation('quote', app()->getLocale()) }}</p>
+    <p class="text-center text-3xl mb-12 break-words">
+        {{ $quote->getTranslation('quote', app()->getLocale()) }}</p>
 
     <a href="/movies/{{ $quote->movie_id }}" class="text-center underline text-5xl">{{ $quote->movie->title }}</a>
 </div>
