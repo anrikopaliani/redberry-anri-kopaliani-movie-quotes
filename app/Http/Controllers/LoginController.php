b030b64b33ff94ecfe8f0fd07d23208c167e489a
anrikopaliani <anrikopa@gmail.com>
2023-04-26 03:54:42 +0400

    fix:show incorrect password error

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -24,7 +24,7 @@ class LoginController extends Controller
 		}
 
 		// if auth failed
-		return back();
+		return back()->withErrors(['password' => __('messages.Incorrect Password')])->withInput(['username' => $validated['username']]);
 	}
 
 	public function destroy(): RedirectResponse
