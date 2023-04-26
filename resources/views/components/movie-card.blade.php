743a0142068d5ac10385bb27d8a7154cce136cdc
anrikopaliani <anrikopa@gmail.com>
2023-04-26 11:39:30 +0400

    fix: quote width

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -3,7 +3,7 @@
 <div class="flex flex-col h-screen items-center" style="width: 1000px">
     <img width="600" height="386px" class="mt-28 mb-6 mx-auto"
         src="{{ URL::to('/') . '/storage/' . str_replace('images/', '', $quote->image) }}" alt="">
-    <p class="text-center text-3xl mb-12 break-words">
+    <p style="width: 900px" class="text-center text-3xl mb-12 break-words">
         {{ $quote->getTranslation('quote', app()->getLocale()) }}</p>
 
     <a href="{{ route('movie.show', ['movie' => $quote->movie_id]) }}"
