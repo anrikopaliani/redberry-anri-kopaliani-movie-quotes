b39a10c07ca2ec10f5b00f15b2cf5ecea6d83216
anrikopaliani <anrikopa@gmail.com>
2023-04-27 20:26:57 +0400

    fix:show images and move it to center

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -1,8 +1,8 @@
 @props(['quote'])
 
-<div class="flex flex-col h-screen items-center" style="width: 1000px">
-    <img width="600" height="386px" class="mt-28 mb-6 mx-auto"
-        src="{{ URL::to('/') . '/storage/' . str_replace('images/', '', $quote->image) }}" alt="">
+<div class="flex flex-col h-screen items-center justify-center" style="width: 1000px">
+    <img width="600" height="386px" class="mt-28 mb-6 mx-auto" src="{{ URL::to('/') . '/storage/' . $quote->image }}"
+        alt="">
     <p class="text-center text-3xl mb-12 break-words">
         {{ $quote->getTranslation('quote', app()->getLocale()) }}</p>
 
