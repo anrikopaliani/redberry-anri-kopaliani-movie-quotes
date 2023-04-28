456fea6b7d02f042342c9fbae429db215e5cc5ba
anrikopaliani <anrikopa@gmail.com>
2023-04-28 14:43:00 +0400

    fix: images url and p width

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -2,9 +2,9 @@
 
 
 <div class="flex flex-col h-screen items-center justify-center" style="width: 1000px">
-    <img width="600" height="386px" class="mt-28 mb-6 mx-auto" src="{{ URL::to('/') . '/storage/' . $quote->image }}"
-        alt="">
-    <p class="text-center text-3xl mb-12 break-words">
+    <img width="600" height="386px" class="mt-28 mb-6 mx-auto"
+        src="{{ URL::to('/') . '/storage/' . 'images/' . $quote->image }}" alt="">
+    <p style="width: 900px" class="text-center text-3xl mb-12 break-words">
         {{ $quote->getTranslation('quote', app()->getLocale()) }}
     </p>
     <a href="{{ route('movie.show', ['movie' => $quote->movie_id]) }}"
